b228fb1be94dd583f98719830dfae28b21ccff17
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-06-09 18:04:59 +0300

    Updated metadata description

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header/Header';
 
 export const metadata = {
   title: 'Clock App',
-  description: 'Simple Clock App',
+  description: 'Simple Clock App, featuring modern stopwatch, timer, and alarm functionalities.',
 };
 
 export const ubuntu = Ubuntu({
